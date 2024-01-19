6db8ec488bc6a302aab71a759dbbd3b9a062f5e1
70928manson <manson972@yahoo.com>
2024-01-19 14:25:12 +0800

    Adding Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Manson Dashboard',
+    default: 'Manson Dashboard',
+  },
+  description: 'The official Next.js Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
+
 export default function RootLayout({
   children,
 }: {
